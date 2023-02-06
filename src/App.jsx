5668248652e60bdb62ee245f9d4f8460ff0524c1
HugoBzn <hugobazan1499@gmail.com>
2023-02-06 13:26:03 -0600

    New localstorage working

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,9 @@ import Header from "./components/Header";
 import ListadoPacientes from "./components/ListadoPacientes";
 
 function App() {
-  const [pacientes, setPacientes] = useState([]);
+  const LS = JSON.parse(localStorage.getItem("pacientes")) ?? [];
+  const [pacientes, setPacientes] = useState(LS);
   const [paciente, setPaciente] = useState({});
-
-  useEffect(() => {
-    const obtenerLS = () => {
-      const pacientesLS = JSON.parse(localStorage.getItem("pacientes")) ?? [];
-      setPacientes(pacientesLS);
-    };
-    obtenerLS();
-  }, []);
 
   useEffect(() => {
     localStorage.setItem("pacientes", JSON.stringify(pacientes));
